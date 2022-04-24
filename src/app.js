d9e55524ea64ed720b8800d7d1f5b627857d7536
mochbili <mochbili@gmail.com>
2022-04-25 02:46:27 +0700

    create express app

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
 'use strict';
 
-const express = require('express');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (request, response) => {
+  response.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
