ed0ffcb74d8406f1fc1a8a933a885e963b7469d0
mochbili <mochbili@gmail.com>
2022-04-25 02:57:24 +0700

    add blog route

--- src/app.js
+++ src/app.js
@@ -3,11 +3,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const posts = require('./mock/posts.json');
 
-app.get('/', (request, response) => {
-  response.send('Hello World!')
-})
+app.get('/', (req, response) => {
+  response.send('<h1>Hello World!</h1>')
+});
+
+app.get('/blog', (req, response) => {
+  response.send(posts);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
