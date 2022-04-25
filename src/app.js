7668ac2a16eaa339740e6a973709b0df98baca7f
mochbili <mochbili@gmail.com>
2022-04-25 22:06:50 +0700

    add static file to layout template

--- src/app.js
+++ src/app.js
@@ -11,25 +11,25 @@ app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
 app.get('/', (req, res) => {
-  res.render('index');
+	res.render('index');
 });
 
 app.get('/blog/:title?', (req, res) => {
-  const title = req.params.title;
+	const title = req.params.title;
 
-  if (title === undefined) {
-    res.status(503);
-    res.send('this page is under construction');
-  } else {
-    // if (title === posts[title]) {
-      const post = posts[title] || {};
-      res.render('post', {post: post});
-    // } else {
-    //   res.render('post', {error: 'error blog not found'});
-    // }
-  }
+	if (title === undefined) {
+		res.status(503);
+		res.send('this page is under construction');
+	} else {
+		// if (title === posts[title]) {
+			const post = posts[title] || {};
+			res.render('post', {post: post});
+		// } else {
+		//   res.render('post', {error: 'error blog not found'});
+		// }
+	}
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+	console.log(`Example app listening at http://localhost:${port}`)
 });
